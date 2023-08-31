e65825eae2d67b5cd3643503aea4667f6b6c79db
tonimontana0328 <luis_weber@gmx.de>
2023-08-31 19:45:46 +0200

    Display 4th weapons slot again.

--- GenerateMod_custom.bat
+++ GenerateMod_custom.bat
@@ -1,15 +1,18 @@
 ECHO //>>"GameData\Generated\Gameplay\Gfx\CapaciteList.ndf"
+
 Rem Changing to the parent directory two levels up
 ECHO cd..\..
 ECHO cd
 ECHO //>>"GameData\Gameplay\Constantes\Ravitaillement.ndf"
+
 Rem Changing to the parent directory two levels up
 ECHO cd..\..
 ECHO cd
 ECHO //>>"GameData\Generated\Gameplay\Gfx\DamageResistance.ndf"
+
 Rem Changing to the parent directory two levels up
 ECHO cd..\..
 ECHO cd
 @echo off
 for %%* in (.) do set CurrDirName=%%~nx*
-..\..\ModGen.exe -headless -datapackonly -strict -generatemod "Mods/%CurrDirName%/" CommonData:Clusters/BootStraps/ClusterBootstrapGeneration.ndf
+..\..\WARNO.exe -headless -datapackonly -generatemod "Mods/%CurrDirName%/" CommonData:Clusters/Bootstrap/ClusterBootstrapGeneration.ndf
